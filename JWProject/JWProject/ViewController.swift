a1ae8d159b49fb4b249b873be5e276a0a606c649
jiwook <xodhks_0113@naver.com>
2021-04-06 21:56:40 +0900

    [developer] Sample 소스 개발 영역 커밋.

--- JWProject/JWProject/ViewController.swift
+++ JWProject/JWProject/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import JWLibrary
+
+public class sample {
+    public static let shared = sample()
+    
+    func temp() {
+        
+    }
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.initDzViews()
     }
+}
 
-
+/// 1. 프로토콜 상속받기!!
+extension ViewController: JWViewProtocol {
+    func initDzViews() {
+        sample.shared.temp()
+    }
+    
+    func updateDzViews() {
+        
+    }
+    
+    func setDzNavigationViews() {
+        
+    }
+    
+    func doDzBackAction() {
+        
+    }
 }
 
