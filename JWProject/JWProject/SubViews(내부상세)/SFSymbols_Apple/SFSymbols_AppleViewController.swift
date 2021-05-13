8a8328bede0e4a93a5e14aab10b454bda3cde2dd
jiwook <xodhks_0113@naver.com>
2021-05-13 19:27:33 +0900

    [SFSymbols] 제작완료.

--- JWProject/JWProject/SubViews(내부상세)/SFSymbols_Apple/SFSymbols_AppleViewController.swift
+++ JWProject/JWProject/SubViews(내부상세)/SFSymbols_Apple/SFSymbols_AppleViewController.swift
@@ -6,13 +6,19 @@
 //
 
 import UIKit
+import JWLibrary
 
 class SFSymbols_AppleViewController: UIViewController {
 
+    //MARK: - IBOutlet
+    /// 코드에서 추가해볼 StackView
+    @IBOutlet weak var codeInsertStackView: UIStackView!
+    /// 코드에서 이름 변경해볼 Label
+    @IBOutlet weak var codeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.initDzViews()
     }
     
     /// 생성자 추가.
@@ -20,4 +26,40 @@ class SFSymbols_AppleViewController: UIViewController {
         let desc = SFSymbols_AppleViewController(nibName: "SFSymbols_AppleViewController", bundle: nil)
         return desc
     }
+}
+
+extension SFSymbols_AppleViewController: JWViewProtocol {
+    func initDzViews() {
+        /*
+         1. UIImage 사용할때 systemImage 을 사용할 수 있는곳에 전부 사용이 가능함.
+         */
+        let commandImage = UIImageView(image: UIImage(systemName: "command"))
+        let optionImage = UIImageView(image: UIImage(systemName: "option"))
+        let altImage = UIImageView(image: UIImage(systemName: "alt"))
+        
+        self.codeInsertStackView.addArrangedSubview(commandImage)
+        self.codeInsertStackView.addArrangedSubview(optionImage)
+        self.codeInsertStackView.addArrangedSubview(altImage)
+        
+        /*
+         2. 이름 영역에 특정 기호 넣어보기 (기호복사를 통해 사용이 가능함)
+         􀆔, 􀆕, 􀆖, 􀆝, 􀆮, 􀖃, 􀇑
+         이것저것 다 복사됨
+         */
+        self.codeLabel.text = "기호복사 [􀆔, 􀆕, 􀆖, 􀆝, 􀆮, 􀖃, 􀇑]"
+    }
+    
+    func updateDzViews() {
+        
+    }
+    
+    func setDzNavigationViews() {
+        
+    }
+    
+    func doDzBackAction() {
+        
+    }
+    
+    
 }
