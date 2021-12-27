027b6731e83aeb6a8390f3df1c604c3be2ad63d5
jiwook <xodhks_0113@naver.com>
2021-12-27 17:53:13 +0900

    [developer] SwiftUI 프로젝트 커밋.

---
+++ JWProject_SwiftUI/JWProject_SwiftUI/JWProject_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JWProject_SwiftUIApp.swift
+//  JWProject_SwiftUI
+//
+//  Created by JW_Macbook on 2021/12/27.
+//
+
+import SwiftUI
+
+@main
+struct JWProject_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
