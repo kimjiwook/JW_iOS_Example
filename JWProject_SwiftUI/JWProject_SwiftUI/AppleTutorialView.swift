93795a100e009b40d45adf11b97f55cb6f37a88f
xodhks0113 <xodhks0113@douzon.com>
2021-12-27 23:25:59 +0900

    [SwiftUI] 샘플 View 추가 생성.

---
+++ JWProject_SwiftUI/JWProject_SwiftUI/AppleTutorialView.swift
@@ -0,0 +1,20 @@
+//
+//  AppleTutorialView.swift
+//  JWProject_SwiftUI
+//
+//  Created by iOS_Mac Mini on 2021/12/27.
+//
+
+import SwiftUI
+
+struct AppleTutorialView: View {
+    var body: some View {
+        Text("Hello, World!_AppleTutorial")
+    }
+}
+
+struct AppleTutorialView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppleTutorialView()
+    }
+}
--- JWProject_SwiftUI/JWProject_SwiftUI/JWProject_SwiftUIApp.swift
+++ JWProject_SwiftUI/JWProject_SwiftUI/JWProject_SwiftUIApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct JWProject_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            AppleTutorialView()
         }
     }
 }
