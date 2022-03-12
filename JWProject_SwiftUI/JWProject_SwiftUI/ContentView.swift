516cefda51f4955483f35cbd50cecf7f31e22a42
Jiwook <xodhks_0113@naver.com>
2022-03-12 23:10:30 +0900

    [SwiftUI] 기능 샘플 개발을 위한 테스트.

--- JWProject_SwiftUI/JWProject_SwiftUI/ContentView.swift
+++ JWProject_SwiftUI/JWProject_SwiftUI/ContentView.swift
@@ -7,10 +7,23 @@
 
 import SwiftUI
 
+/// 최초 시작 뷰
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVStack {
+                ContentItemView()
+            }
+        }
+    }
+}
+
+/// Item View
+struct ContentItemView:View {
+    var body: some View {
+        HStack {
+            Text("4444")
+        }
     }
 }
 
